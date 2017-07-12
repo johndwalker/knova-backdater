b6f94ce4b8ce184cb22aa3bbe50bcc9a503d8fc1
johndwalker <johnathonwalker@icloud.com>
2017-07-12 10:34:51 -0600

    Reads file to string, parses csv, stores as array

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var csvParser = require('./lib/csvParser')
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end();
 }).listen(8080);
+
+csvParser.readFile('test/tid_date.csv');
